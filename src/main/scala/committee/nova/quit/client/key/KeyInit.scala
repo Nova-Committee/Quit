2a0eb17c40ef3583008cb2518c36e214281eb7fd
MikhailTapio <mikhailtapio13468@163.com>
2022-10-04 09:58:02 +0800

    Avoid key conflict

--- src/main/scala/committee/nova/quit/client/key/KeyInit.scala
+++ src/main/scala/committee/nova/quit/client/key/KeyInit.scala
@@ -1,12 +1,13 @@
 package committee.nova.quit.client.key
 
 import net.minecraft.client.settings.KeyBinding
+import net.minecraftforge.client.settings.KeyConflictContext
 import net.minecraftforge.fml.client.registry.ClientRegistry
 import org.lwjgl.input.Keyboard
 
 object KeyInit {
-  val fastQuit = new KeyBinding("key.quit.fastQuit", Keyboard.KEY_Q, "key.categories.quit")
-  val bossKey = new KeyBinding("key.quit.bossKey", Keyboard.KEY_B, "key.categories.quit")
+  val fastQuit = new KeyBinding("key.quit.fastQuit", KeyConflictContext.GUI, Keyboard.KEY_Q, "key.categories.quit")
+  val bossKey = new KeyBinding("key.quit.bossKey", KeyConflictContext.GUI, Keyboard.KEY_B, "key.categories.quit")
 
   def init(): Unit = {
     ClientRegistry.registerKeyBinding(fastQuit)
